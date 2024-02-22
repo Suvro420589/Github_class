b47dd2adce4c713a47ca0755a3ef1f7cf0856ca0
Suvro420589 <110317623+Suvro420589@users.noreply.github.com>
2024-02-22 19:50:18 +0530

    Add files via upload

---
+++ char_location.java
@@ -0,0 +1,17 @@
+class Charloc
+{
+	public static void main(String args[])
+	{
+		String str1 = new String("Maa Baba");
+		String str2 = new String("Love you");
+		char r1 = str1.charAt(2);
+		char r2 = str2.charAt(7);
+		char r3 = str1.charAt(4);
+		char r4 = str2.charAt(5);
+
+		System.out.println(r2);
+		System.out.println(r1);
+		System.out.println(r4);
+		System.out.println(r3);
+	}
+}
---
+++ charactercase.java
@@ -0,0 +1,13 @@
+class Char
+{
+	public static void main(String args[])
+	{
+		String str1 = new String("Suvro");
+		String str2 = new String("SUVRO");
+		String x = str1.toUpperCase();
+		String y = str2.toLowerCase();
+		
+		System.out.println("x = "+x);
+		System.out.println("y = "+y);
+	}
+}
